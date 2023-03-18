2487265abfb31973a7308a0196f4e8f06604e443
cunyu1943 <747731461@qq.com>
2023-03-18 22:15:27 +0800

    Java 概述课后练习

---
+++ java-tutorial/java-tutorial-01/src/io/github/cunyu1943/Heart.java
@@ -0,0 +1,27 @@
+package io.github.cunyu1943;
+
+/**
+ * 打印 I Love You 爱心
+ *
+ * @author : 村雨遥
+ * @version : 1.0
+ * @package : io.github.cunyu1943
+ * @className : Heart.java
+ * @createTime : 2023/3/18 22:09
+ * @email : 747731461@qq.com
+ * @公众号 : 村雨遥
+ * @website : https://cunyu1943.github.io/JavaPark
+ */
+public class Heart {
+    public static void main(String[] args) {
+        System.out.print("\t" + "*" + "\t\t\t\t\t\t\t\t\t\t\t\t" + "*" + "\t" + "\n");
+        System.out.print("*" + "\t\t" + "*" + "\t\t\t\t" + "I love you" + "\t\t\t\t" + " * " + "\t\t" + " * " + "\n");
+        System.out.print("\t" + "*" + "\t\t\t\t\t\t\t\t\t\t\t\t" + "*" + "\t" + "\n");
+        System.out.print("\t\t" + "*" + "\t\t\t\t\t\t\t\t\t\t" + "*" + "\t\t" + "\n");
+        System.out.print("\t\t\t" + "*" + "\t\t\t\t\t\t\t\t" + "*" + "\t" + "\n");
+        System.out.print("\t\t\t\t" + "*" + "\t\t\t\t\t\t" + "*" + "" + "\t" + "\n");
+        System.out.print("\t\t\t\t\t" + "*" + "\t\t\t\t" + "*" + "" + "\t\t" + "\n");
+        System.out.print("\t\t\t\t\t\t" + "*" + "\t\t" + "*" + "" + "\t\t" + "\n");
+        System.out.print("\t\t\t\t\t\t\t" + "*" + "\n");
+    }
+}
---
+++ java-tutorial/java-tutorial-01/src/io/github/cunyu1943/HelloJava.java
@@ -0,0 +1,19 @@
+package io.github.cunyu1943;
+
+/**
+ * 课后练习 1
+ *
+ * @author : 村雨遥
+ * @version : 1.0
+ * @package : io.github.cunyu1943
+ * @className : HelloJava.java
+ * @createTime : 2023/3/18 22:04
+ * @email : 747731461@qq.com
+ * @公众号 : 村雨遥
+ * @website : https://cunyu1943.github.io/JavaPark
+ */
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.println("Hello Java，学 Java 认准 JavaPark!");
+    }
+}
