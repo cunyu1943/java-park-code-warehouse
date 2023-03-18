7d82d60b6684a02779b2cea7e3bf8e8a2cddc757
cunyu1943 <747731461@qq.com>
2023-03-18 21:57:07 +0800

    java-tutorial-01

---
+++ java-tutorial/java-tutorial-01/src/io/github/cunyu1943/HelloWorld.java
@@ -0,0 +1,19 @@
+package io.github.cunyu1943;
+
+/**
+ * Hello World 入门程序
+ *
+ * @author : 村雨遥
+ * @version : 1.0
+ * @package : PACKAGE_NAME
+ * @className : io.github.cunyu1943.HelloWorld.java
+ * @createTime : 2023/3/18 21:51
+ * @email : 747731461@qq.com
+ * @公众号 : 村雨遥
+ * @website : https://cunyu1943.github.io/JavaPark
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
